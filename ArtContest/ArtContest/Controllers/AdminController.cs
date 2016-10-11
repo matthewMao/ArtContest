16c60c8477dcb8bf17b0b36be5c3c45748fa0b78
yangbyron <yangbyron998@gmail.com>
2016-10-11 15:56:11 -0700

    bug fix

--- ArtContest/ArtContest/Controllers/AdminController.cs
+++ ArtContest/ArtContest/Controllers/AdminController.cs
@@ -16,7 +16,7 @@ namespace ArtContest.Controllers
         {
             CTEFArtContestEntities dbc = new CTEFArtContestEntities();
             
-            List<Picture> pics= dbc.Pictures.Include("Student").Where(p => p.Public.Equals("yes")).ToList();
+            List<Picture> pics= dbc.Pictures.Include("Student").Where(p => p.Public.Equals("Yes")).ToList();
             return View(pics);
         }
         public ActionResult DeleteMission(int id) {
@@ -41,30 +41,29 @@ namespace ArtContest.Controllers
             return View(vm);
         }
         [HttpPost]
-        public ActionResult DividePic(int judgeId,string grade) {
-            CTEFArtContestEntities dbc = new CTEFArtContestEntities();
-            List<Student> stus=dbc.Students.Where(s => s.Grade == grade).ToList();
-            int amount = stus.Count();
-            foreach(var s in stus) {
-               Picture pic= dbc.Pictures.Where(p => p.UserId == s.Id&&p.Public.Equals("Yes")).SingleOrDefault();
-                PictureRate pr = new PictureRate();
-                pr.PictureId = pic.Id;
-                pr.JudgeId = judgeId;
-                pr.Rate = 0;
-                if(!dbc.PictureRates.Any(r => r.JudgeId == judgeId && r.PictureId == pr.PictureId)) { 
-                dbc.PictureRates.Add(pr);
-                dbc.SaveChanges();
-                    amount--;
-                }else {
-                    ModelState.AddModelError("You have Assign These Pictures to This Judge","You have Assign These Pictures to This Judge");
+        public ActionResult DividePic(int judgeId,string[] Grade) {
+            foreach(var grade in Grade) {
+                CTEFArtContestEntities dbc = new CTEFArtContestEntities();
+                List<Student> stus = dbc.Students.Where(s => s.Grade == grade).ToList();
+                int amount = stus.Count();
+                foreach(var s in stus) {
+                    Picture pic = dbc.Pictures.Where(p => p.UserId == s.Id && p.Public.Equals("Yes")).SingleOrDefault();
+                    PictureRate pr = new PictureRate();
+                    pr.PictureId = pic.Id;
+                    pr.JudgeId = judgeId;
+                    pr.Rate = 0;
+                    if(!dbc.PictureRates.Any(r => r.JudgeId == judgeId && r.PictureId == pr.PictureId)) {
+                        dbc.PictureRates.Add(pr);
+                        dbc.SaveChanges();
+                        amount--;
+                    } else {
+                        ModelState.AddModelError("You have Assign These Pictures to This Judge","You have Assign These Pictures to This Judge");
+                    }
                 }
+
                 
-                if(amount == 0) {
-                    return RedirectToAction("Index","Admin");
-                }  
             }
-            
-            return RedirectToAction("Index","Admin");          
+                  return RedirectToAction("Index","Admin");
         }
         public ActionResult ViewJudge() {
             CTEFArtContestEntities dbc = new CTEFArtContestEntities();
@@ -101,17 +100,17 @@ namespace ArtContest.Controllers
         }
         public ActionResult SearchBySchool(string school) {
             CTEFArtContestEntities dbc = new CTEFArtContestEntities();
-            List<Picture> pics = dbc.Pictures.Include("Student").Where(p => p.Student.School.Equals(school) && p.Public.Equals("yes")).ToList();
+            List<Picture> pics = dbc.Pictures.Include("Student").Where(p => p.Student.School.Equals(school) && p.Public.Equals("Yes")).ToList();
             return View("Index",pics);
         }
         public ActionResult SearchByGrade(string grade) {
             CTEFArtContestEntities dbc = new CTEFArtContestEntities();
-            List<Picture> pics = dbc.Pictures.Include("Student").Where(p => p.Student.Grade.Equals(grade) && p.Public.Equals("yes")).ToList();
+            List<Picture> pics = dbc.Pictures.Include("Student").Where(p => p.Student.Grade.Equals(grade) && p.Public.Equals("Yes")).ToList();
             return View("Index",pics);
         }
         public ActionResult SearchBySchoolThenByGrade(string school, string grade) {
             CTEFArtContestEntities dbc = new CTEFArtContestEntities();
-            List<Picture> pics = dbc.Pictures.Include("Student").Where(p => p.Student.School.Equals(school) && p.Student.Grade.Equals(grade) && p.Public.Equals("yes")).ToList();
+            List<Picture> pics = dbc.Pictures.Include("Student").Where(p => p.Student.School.Equals(school) && p.Student.Grade.Equals(grade) && p.Public.Equals("Yes")).ToList();
             return View("Index",pics);
         }
     }
