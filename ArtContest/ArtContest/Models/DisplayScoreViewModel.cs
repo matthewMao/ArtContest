c42678a1103956eb2e57005023e54dd30ac41e76
yangbyron <yangbyron998@gmail.com>
2016-10-15 13:34:37 -0700

    none

---
+++ ArtContest/ArtContest/Models/DisplayScoreViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ArtContest.Models {
+    public class DisplayScoreViewModel {
+        public List<Picture> Pictures { get; set; }
+        public List<PictureRate> PictureRates { get; set; }
+    }
+}
